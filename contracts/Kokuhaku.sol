8327da0b55b7e3e22f9b7355ed18090ae5a4f024
Daniel Ocampo <96505706+da-ocampo@users.noreply.github.com>
2024-05-24 01:19:43 -0500

    Added "setBaseURI"

--- contracts/Kokuhaku.sol
+++ contracts/Kokuhaku.sol
@@ -9,10 +9,12 @@ import {ERC721Pausable} from "@openzeppelin/contracts/token/ERC721/extensions/ER
 import {ERC2981} from "@openzeppelin/contracts/token/common/ERC2981.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
-import {IKokuhaku} from "./IKokuhaku.sol";
+import {IKokuhaku} from "/IKokuhaku.sol";
 
-/// @title Kokuhaku NFT Contract
-/// @notice This contract implements an ERC721 NFT with minting, burning, pausing, and royalty functionalities.
+/**
+ * @title Kokuhaku
+ * @dev Implementation of the Kokuhaku ERC721 NFT contract with additional features such as pausing, burning, royalty settings, and whitelist functionality.
+ */
 contract Kokuhaku is
     IKokuhaku,
     ERC721,
@@ -24,38 +26,25 @@ contract Kokuhaku is
 {
     using BitMaps for BitMaps.BitMap;
 
-    // ✧･ﾟ: *✧･ﾟ:* 2. Property Variables ✧･ﾟ: *✧･ﾟ:*
-    /// @notice Maximum supply of tokens.
     uint96 public constant maxSupply = 20000;
-
-    /// @notice Base URI for token metadata.
     string public baseURI;
-
-    /// @notice URI for the contract metadata.
     string public contractURI;
-
-    /// @dev Internal counter for the next token ID.
     uint256 private _nextTokenId;
-
     BitMaps.BitMap internal envelopeOpened;
-
-    /// @dev Mapping to track if an envelope is opened.
-    // mapping(uint256 => bool) private envelopeOpened;
-
-    /// @notice Mapping to track the whitelist status of addresses.
     mapping(address => bool) public whiteList;
 
-    // ✧･ﾟ: *✧･ﾟ:* 3. Constructor ✧･ﾟ: *✧･ﾟ:*
-    /// @notice Constructor to initialize the contract with initial values.
-    /// @param initialOwner Address of the initial owner of the contract.
-    /// @param feeNumerator Royalty fee numerator.
-    /// @param _initBaseURI Initial base URI for token metadata.
-    /// @param _contractURI URI for the contract metadata.
+    /**
+     * @dev Initializes the contract by setting the initial owner, royalty fee, base URI, and contract URI.
+     * @param initialOwner The address of the initial owner.
+     * @param feeNumerator The numerator for the royalty fee.
+     * @param initBaseURI The initial base URI for token metadata.
+     * @param contractURI_ The URI for the contract metadata.
+     */
     constructor(
         address initialOwner,
         uint96 feeNumerator,
-        string memory _initBaseURI,
-        string memory _contractURI
+        string memory initBaseURI,
+        string memory contractURI_
     ) payable ERC721("Kokuhaku", "KOKU") Ownable(initialOwner) {
         if (initialOwner == address(0)) {
             revert ZeroAddressDisallowed();
@@ -63,16 +52,16 @@ contract Kokuhaku is
 
         require(feeNumerator <= _feeDenominator(), "Invalid fee numerator");
 
-        if (bytes(_initBaseURI).length > 0) {
+        if (bytes(initBaseURI).length == 0) {
             revert EmptyUriDisallowed();
         }
 
-        if (bytes(_contractURI).length > 0) {
+        if (bytes(contractURI_).length == 0) {
             revert EmptyUriDisallowed();
         }
 
-        baseURI = _initBaseURI;
-        contractURI = _contractURI;
+        baseURI = initBaseURI;
+        contractURI = contractURI_;
 
         _setDefaultRoyalty(initialOwner, feeNumerator);
 
@@ -81,30 +70,37 @@ contract Kokuhaku is
         }
     }
 
-    // ✧･ﾟ: *✧･ﾟ:* 4. Owner Functions ✧･ﾟ: *✧･ﾟ:*
-    /// @notice Function to pause the contract.
-    /// @dev Can only be called by the owner.
+    /**
+     * @notice Pauses all token transfers.
+     * @dev Only callable by the owner.
+     */
     function pause() external onlyOwner {
         _pause();
     }
 
-    /// @notice Function to unpause the contract.
-    /// @dev Can only be called by the owner.
+    /**
+     * @notice Unpauses all token transfers.
+     * @dev Only callable by the owner.
+     */
     function unpause() external onlyOwner {
         _unpause();
     }
 
-    /// @notice Function to set the contract URI.
-    /// @dev Can only be called by the owner.
-    /// @param _contractURI New contract URI.
-    function setContractURI(string calldata _contractURI) external onlyOwner {
-        contractURI = _contractURI;
+    /**
+     * @notice Sets the contract URI.
+     * @dev Only callable by the owner.
+     * @param contractURI_ The new contract URI.
+     */
+    function setContractURI(string calldata contractURI_) external onlyOwner {
+        contractURI = contractURI_;
     }
 
-    /// @notice Function to reset the royalty information.
-    /// @dev Can only be called by the owner.
-    /// @param receiver Address to receive the royalties.
-    /// @param feeNumerator Royalty fee numerator.
+    /**
+     * @notice Resets the royalty settings.
+     * @dev Only callable by the owner.
+     * @param receiver The address of the royalty receiver.
+     * @param feeNumerator The numerator for the royalty fee.
+     */
     function resetRoyalty(
         address receiver,
         uint96 feeNumerator
@@ -112,13 +108,15 @@ contract Kokuhaku is
         _setDefaultRoyalty(receiver, feeNumerator);
     }
 
-    /// @notice Function to withdraw the contract's balance to a specified address.
-    /// @dev Can only be called by the owner.
-    /// @param _addr Address to receive the withdrawn balance.
-    function withdraw(address _addr) external onlyOwner {
+    /**
+     * @notice Withdraws the entire balance of the contract to a specified address.
+     * @dev Only callable by the owner.
+     * @param addr The address to send the balance to.
+     */
+    function withdraw(address addr) external onlyOwner {
         bytes4 errorSelector = IKokuhaku.TransferFailed.selector;
         assembly {
-            let success := call(gas(), _addr, selfbalance(), 0, 0, 0, 0)
+            let success := call(gas(), addr, selfbalance(), 0, 0, 0, 0)
             if iszero(success) {
                 let ptr := mload(0x40)
                 mstore(ptr, errorSelector)
@@ -127,9 +125,11 @@ contract Kokuhaku is
         }
     }
 
-    /// @notice Function to set the whitelist addresses.
-    /// @dev Can only be called by the owner.
-    /// @param addresses Array of addresses to be whitelisted.
+    /**
+     * @notice Adds a list of addresses to the whitelist.
+     * @dev Only callable by the owner.
+     * @param addresses The list of addresses to add to the whitelist.
+     */
     function setWhiteList(address[] calldata addresses) external onlyOwner {
         uint256 length = addresses.length;
         for (uint256 i; i < length; i++) {
@@ -137,69 +137,82 @@ contract Kokuhaku is
         }
     }
 
-    // ✧･ﾟ: *✧･ﾟ:* 5. Minting Functions ✧･ﾟ: *✧･ﾟ:*
-    /// @notice Function for whitelisted addresses to mint a token for free.
-    /// @dev Only whitelisted addresses can call this function.
+    /**
+     * @notice Sets the base URI for token metadata.
+     * @dev Only callable by the owner.
+     * @param newBaseURI The new base URI.
+     */
+    function setBaseURI(string calldata newBaseURI) external onlyOwner {
+        baseURI = newBaseURI;
+    }
+
+    /**
+     * @notice Mints a token if the caller is on the whitelist.
+     * @dev Only callable when not paused.
+     * Emits a {FreeMintUsed} event.
+     */
     function privateMint() external whenNotPaused {
         if (!whiteList[msg.sender]) revert NotOnWhiteList();
-
         if (totalSupply() == maxSupply) revert MintingExceedsMaxSupply();
-
         delete whiteList[msg.sender];
         mint(msg.sender);
         emit FreeMintUsed(msg.sender);
     }
 
-    /// @notice Function for public to mint a token with a fee.
-    /// @dev Requires payment of 1 ether.
+    /**
+     * @notice Mints a token to the caller.
+     * @dev Only callable when not paused. Requires a payment of 1 ether.
+     */
     function publicMint() external payable whenNotPaused {
-        if (msg.value != 1.00 ether) revert NotEnoughFunds();
+        if (msg.value != 1 ether) revert NotEnoughFunds();
         if (totalSupply() == maxSupply) revert MintingExceedsMaxSupply();
         mint(msg.sender);
     }
 
-    /// @notice Function for batch minting of tokens.
-    /// @dev Requires payment of 1 ether per token and a maximum of 10 tokens can be minted in a single batch.
-    /// @param amount Number of tokens to mint.
-    function batchMint(
-        uint256 amount
-    ) external payable whenNotPaused whenNotPaused {
+    /**
+     * @notice Mints multiple tokens to the caller.
+     * @dev Only callable when not paused. Requires a payment of 1 ether per token.
+     * @param amount The number of tokens to mint.
+     */
+    function batchMint(uint256 amount) external payable whenNotPaused {
         if (amount == 0) revert CannotMintZeroTokens();
         if (amount > 10) revert ExceedsBatchLimit();
-        if (totalSupply() + amount > maxSupply)
-            revert MintingExceedsMaxSupply();
-        if (msg.value != 1.00 ether * amount) revert NotEnoughFunds();
-
+        if (totalSupply() + amount > maxSupply) revert MintingExceedsMaxSupply();
+        if (msg.value != 1 ether * amount) revert NotEnoughFunds();
         for (uint256 i; i < amount; i++) {
             mint(msg.sender);
         }
     }
 
-    /// @notice Function for the owner to mint tokens to multiple recipients.
-    /// @dev Can only be called by the owner.
-    /// @param recipients Array of addresses to receive the minted tokens.
+    /**
+     * @notice Mints tokens to a list of recipients.
+     * @dev Only callable by the owner.
+     * @param recipients The list of addresses to receive tokens.
+     */
     function airdropMint(address[] calldata recipients) external onlyOwner {
         uint256 recipientsLength = recipients.length;
         if (totalSupply() + recipientsLength > maxSupply)
             revert MintingExceedsMaxSupply();
-
         for (uint256 i; i < recipientsLength; i++) {
             mint(recipients[i]);
         }
     }
 
-    /// @notice Internal function to mint a token to a specified address.
-    /// @param to Address to receive the minted token.
+    /**
+     * @dev Mints a token to a specified address.
+     * @param to The address to mint the token to.
+     */
     function mint(address to) internal {
         _safeMint(to, _nextTokenId++);
     }
 
-    // ✧･ﾟ: *✧･ﾟ:* 6. Transfer Functions ✧･ﾟ: *✧･ﾟ:*
-    /// @notice Function to transfer a token from one address to another.
-    /// @dev Overrides the default ERC721 implementation to include envelope opening logic.
-    /// @param from Address transferring the token.
-    /// @param to Address receiving the token.
-    /// @param tokenId ID of the token being transferred.
+    /**
+     * @notice Transfers a token from one address to another.
+     * @dev Overrides the ERC721 implementation to include envelope opening logic.
+     * @param from The address to transfer the token from.
+     * @param to The address to transfer the token to.
+     * @param tokenId The ID of the token to transfer.
+     */
     function transferFrom(
         address from,
         address to,
@@ -211,12 +224,14 @@ contract Kokuhaku is
         super.transferFrom(from, to, tokenId);
     }
 
-    /// @notice Function to safely transfer a token from one address to another with additional data.
-    /// @dev Overrides the default ERC721 implementation to include envelope opening logic.
-    /// @param from Address transferring the token.
-    /// @param to Address receiving the token.
-    /// @param tokenId ID of the token being transferred.
-    /// @param data Additional data sent with the transfer.
+    /**
+     * @notice Safely transfers a token from one address to another.
+     * @dev Overrides the ERC721 implementation to include envelope opening logic.
+     * @param from The address to transfer the token from.
+     * @param to The address to transfer the token to.
+     * @param tokenId The ID of the token to transfer.
+     * @param data Additional data with no specified format.
+     */
     function safeTransferFrom(
         address from,
         address to,
@@ -229,36 +244,39 @@ contract Kokuhaku is
         super.safeTransferFrom(from, to, tokenId, data);
     }
 
-    // ✧･ﾟ: *✧･ﾟ:* 7. Other Functions ✧･ﾟ: *✧･ﾟ:*
-    /// @notice Function to burn a token.
-    /// @dev Overrides the default ERC721 implementation.
-    /// @param tokenId ID of the token to burn.
+    /**
+     * @notice Burns a token.
+     * @dev Only callable when not paused.
+     * @param tokenId The ID of the token to burn.
+     */
     function burn(uint256 tokenId) public override whenNotPaused {
         super._burn(tokenId);
     }
 
-    /// @notice Function to get the base URI for token metadata.
-    /// @dev Overrides the default ERC721 implementation.
-    /// @return Base URI string.
+    /**
+     * @dev Returns the base URI for token metadata.
+     */
     function _baseURI() internal view override returns (string memory) {
         return baseURI;
     }
 
-    /// @notice Internal function to open an envelope.
-    /// @param tokenId ID of the token whose envelope is to be opened.
+    /**
+     * @dev Opens the envelope associated with a token.
+     * @param tokenId The ID of the token.
+     */
     function openEnvelope(uint256 tokenId) private {
         if (!envelopeOpened.get(tokenId)) {
             envelopeOpened.set(tokenId);
         }
     }
 
-    // ✧･ﾟ: *✧･ﾟ:* 8. Mandatory Overrides ✧･ﾟ: *✧･ﾟ:*
-    /// @notice Internal function to update balances and other data.
-    /// @dev Overrides multiple inherited functions.
-    /// @param to Address receiving the token.
-    /// @param tokenId ID of the token being transferred.
-    /// @param auth Address authorized for the transfer.
-    /// @return Address authorized for the transfer.
+    /**
+     * @dev Updates the ownership and state of a token.
+     * @param to The address to transfer the token to.
+     * @param tokenId The ID of the token to transfer.
+     * @param auth The address authorized to transfer the token.
+     * @return The address of the new owner.
+     */
     function _update(
         address to,
         uint256 tokenId,
@@ -271,10 +289,11 @@ contract Kokuhaku is
         return super._update(to, tokenId, auth);
     }
 
-    /// @notice Function to get the token URI.
-    /// @dev Overrides the default ERC721 implementation.
-    /// @param tokenId ID of the token.
-    /// @return Token URI string.
+    /**
+     * @notice Returns the URI for a token's metadata.
+     * @param tokenId The ID of the token.
+     * @return The URI for the token's metadata.
+     */
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
@@ -288,10 +307,11 @@ contract Kokuhaku is
             );
     }
 
-    /// @notice Internal function to increase balance.
-    /// @dev Overrides multiple inherited functions.
-    /// @param account Address whose balance is to be increased.
-    /// @param value Value by which the balance is to be increased.
+    /**
+     * @dev Increases the balance of an account.
+     * @param account The account to increase the balance of.
+     * @param value The amount to increase the balance by.
+     */
     function _increaseBalance(
         address account,
         uint128 value
@@ -299,10 +319,11 @@ contract Kokuhaku is
         super._increaseBalance(account, value);
     }
 
-    /// @notice Function to check if the contract supports a given interface.
-    /// @dev Overrides multiple inherited functions.
-    /// @param interfaceId Interface identifier.
-    /// @return Boolean indicating if the interface is supported.
+    /**
+     * @notice Checks if the contract supports a specific interface.
+     * @param interfaceId The interface identifier.
+     * @return True if the contract supports the interface, false otherwise.
+     */
     function supportsInterface(
         bytes4 interfaceId
     ) public view override(ERC721, ERC721Enumerable, ERC2981) returns (bool) {
